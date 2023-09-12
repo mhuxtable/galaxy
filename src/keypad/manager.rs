dc0b4d02d25039b930dea5b7ec4bfb3b63b8f497
Matthew Huxtable <git@tigermatt.uk>
2023-09-12 22:21:05 +0100

    keypad manager: pull BacklightResponder to own module, add tests

--- src/keypad/manager.rs
+++ src/keypad/manager.rs
@@ -5,10 +5,7 @@ use std::{
 };
 
 use log::debug;
-use tokio::{
-    sync::{mpsc, oneshot},
-    time::{self, Interval},
-};
+use tokio::time::Interval;
 
 use crate::serial::devices::keypad::{Backlight, Event, EventType, SerialKeypad};
 
@@ -38,6 +35,8 @@ impl KeypadManager {
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn Error + Sync + Send>> {
+        use backlight_responder::BacklightResponder;
+
         let mut event_ch = self.keypad.subscribe_events();
         let mut time_updater_interval = interval_at_next_minute();
 
@@ -160,81 +159,187 @@ impl KeypadManager {
     }
 }
 
-struct BacklightResponder {
-    rx: mpsc::UnboundedReceiver<DisplayMode>,
-    last_state: Option<DisplayMode>,
-    backlight_control: Arc<Box<dyn Fn(Backlight) + Send + Sync>>,
-    timeout: Duration,
-}
+mod backlight_responder {
+    use log::debug;
+    use std::{error::Error, sync::Arc, time::Duration};
+    use tokio::{
+        sync::{mpsc, oneshot},
+        time,
+    };
 
-impl BacklightResponder {
-    fn new(
-        controller: Box<dyn Fn(Backlight) + Send + Sync>,
-    ) -> (BacklightResponder, mpsc::UnboundedSender<DisplayMode>) {
-        let (tx, rx) = mpsc::unbounded_channel();
+    use crate::serial::devices::keypad::Backlight;
 
-        (
-            BacklightResponder {
-                rx,
-                last_state: None,
-                backlight_control: Arc::new(controller),
-                timeout: Duration::from_secs(5),
-            },
-            tx,
-        )
+    use super::DisplayMode;
+
+    pub(super) struct BacklightResponder {
+        rx: mpsc::UnboundedReceiver<DisplayMode>,
+        last_state: Option<DisplayMode>,
+        backlight_control: Arc<Box<dyn Fn(Backlight) + Send + Sync>>,
+        timeout: Duration,
     }
 
-    async fn run(&mut self) -> Result<(), Box<dyn Error + Sync + Send>> {
-        let mut cancel_token: Option<oneshot::Sender<()>> = None;
+    impl BacklightResponder {
+        pub fn new(
+            controller: Box<dyn Fn(Backlight) + Send + Sync>,
+        ) -> (BacklightResponder, mpsc::UnboundedSender<DisplayMode>) {
+            let (tx, rx) = mpsc::unbounded_channel();
 
-        loop {
-            match self.rx.recv().await {
-                Some(display_mode) => {
-                    if self
-                        .last_state
-                        .is_some_and(|last_state| last_state == display_mode)
-                    {
-                        continue;
-                    }
-
-                    self.last_state = Some(display_mode);
-
-                    if let Some(cancel_token) = cancel_token.take() {
-                        debug!("BacklightResponder: cancelling last task as entering new state");
-                        let _ = cancel_token.send(());
-                    }
-
-                    match display_mode {
-                        DisplayMode::Idle => {
-                            // Start a timer to switch off the backlight after a period of time.
-                            let (cancel_tx, cancel_rx) = oneshot::channel();
-                            cancel_token = Some(cancel_tx);
-
-                            let backlight_control = self.backlight_control.clone();
-                            debug!("BacklightResponder: spawning task to toggle backlight state after quiescent period");
-
-                            let timeout_duration = self.timeout.clone();
-
-                            tokio::spawn(async move {
-                                tokio::select! {
-                                    _ = time::sleep(timeout_duration) => {
-                                        debug!("BacklightResponder: toggling backlight as timer fired");
-                                        backlight_control(Backlight::Off);
-                                    }
-                                    _ = cancel_rx => {},
-                                }
-                            });
-                        }
-                        // In any state change away from Idle, the cancel_token was already
-                        // cancelled earlier.
-                        _ => {}
-                    }
-                }
-                None => break,
-            }
+            (
+                BacklightResponder {
+                    rx,
+                    last_state: None,
+                    backlight_control: Arc::new(controller),
+                    timeout: Duration::from_secs(5),
+                },
+                tx,
+            )
         }
 
-        Ok(())
+        pub async fn run(&mut self) -> Result<(), Box<dyn Error + Sync + Send>> {
+            let mut cancel_token: Option<oneshot::Sender<()>> = None;
+
+            loop {
+                match self.rx.recv().await {
+                    Some(display_mode) => {
+                        if self
+                            .last_state
+                            .is_some_and(|last_state| last_state == display_mode)
+                        {
+                            continue;
+                        }
+
+                        self.last_state = Some(display_mode);
+
+                        if let Some(cancel_token) = cancel_token.take() {
+                            debug!(
+                                "BacklightResponder: cancelling last task as entering new state"
+                            );
+                            let _ = cancel_token.send(());
+                        }
+
+                        match display_mode {
+                            DisplayMode::Idle => {
+                                // Start a timer to switch off the backlight after a period of time.
+                                let (cancel_tx, cancel_rx) = oneshot::channel();
+                                cancel_token = Some(cancel_tx);
+
+                                let backlight_control = self.backlight_control.clone();
+                                debug!("BacklightResponder: spawning task to toggle backlight state after quiescent period");
+
+                                let timeout_duration = self.timeout.clone();
+
+                                tokio::spawn(async move {
+                                    tokio::select! {
+                                        _ = time::sleep(timeout_duration) => {
+                                            debug!("BacklightResponder: toggling backlight as timer fired");
+                                            backlight_control(Backlight::Off);
+                                        }
+                                        _ = cancel_rx => {},
+                                    }
+                                });
+                            }
+                            // In any state change away from Idle, the cancel_token was already
+                            // cancelled earlier.
+                            _ => {}
+                        }
+                    }
+                    None => break,
+                }
+            }
+
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use std::sync::{Arc, Mutex};
+        use tokio::{sync::Notify, time};
+
+        use super::*;
+
+        fn instantiate_backlight_responder() -> (
+            Arc<Mutex<Option<Backlight>>>,
+            Arc<Notify>,
+            BacklightResponder,
+            mpsc::UnboundedSender<DisplayMode>,
+        ) {
+            let tx_backlight_state = Arc::new(Mutex::new(None));
+            let notify = Arc::new(Notify::new());
+            let (backlight_responder, tx) = {
+                let state = tx_backlight_state.clone();
+                let notify = notify.clone();
+
+                BacklightResponder::new(Box::new(move |new_state| {
+                    *state.lock().unwrap() = Some(new_state);
+                    notify.notify_waiters();
+                }))
+            };
+
+            (tx_backlight_state, notify, backlight_responder, tx)
+        }
+
+        #[tokio::test]
+        async fn test_backlight_goes_off_after_idle() {
+            time::pause();
+
+            let (state, notify, mut responder, tx) = instantiate_backlight_responder();
+
+            tokio::spawn(async move {
+                responder.run().await.unwrap();
+            });
+
+            tx.send(DisplayMode::Idle).unwrap();
+
+            let tx_backlight_state_clone = state.clone();
+
+            // Resume time to trigger the timer
+            time::advance(Duration::from_secs(5)).await;
+            notify.notified().await;
+
+            assert_eq!(
+                *tx_backlight_state_clone.lock().unwrap(),
+                Some(Backlight::Off)
+            );
+        }
+
+        #[tokio::test]
+        async fn test_backlight_stays_on_if_not_idle() {
+            time::pause();
+
+            let (state, notify, mut responder, tx) = instantiate_backlight_responder();
+            *state.lock().unwrap() = Some(Backlight::On);
+
+            let handle = tokio::spawn(async move {
+                responder.run().await.unwrap();
+            });
+
+            tx.send(DisplayMode::Idle).unwrap();
+            tx.send(DisplayMode::Menu).unwrap();
+
+            let tx_backlight_state_clone = state.clone();
+
+            // Resume time to trigger the timer
+            time::advance(Duration::from_secs(6)).await;
+            drop(tx);
+
+            assert!(handle.await.is_ok());
+            tokio::time::sleep(Duration::from_secs(1)).await;
+
+            assert_eq!(
+                *tx_backlight_state_clone.lock().unwrap(),
+                Some(Backlight::On)
+            );
+        }
+
+        #[tokio::test]
+        async fn test_receiver_closes_loop_breaks() {
+            let (mut backlight_responder, tx) = BacklightResponder::new(Box::new(|_| {}));
+
+            drop(tx); // simulate receiver closing
+
+            assert_eq!(backlight_responder.run().await.is_ok(), true);
+        }
     }
 }
 
